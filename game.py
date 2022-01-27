7c9663b5115d829db0c1229788aacd39ef49a128
AbelSL <abelsl1999@gmail.com>
2022-01-26 22:27:40 -0600

    Se añadió la oración junto con sus espacios y variables

---
+++ game.py
@@ -0,0 +1,5 @@
+peli_fav = input("Pelicula favorita: ")
+adjetivo = input("Adjetivo: ")
+verbo = input("Verbo: ")
+
+madlibs = f"Mi pelicula favorita es {peli_fav} pues es muy {adjetivo} que no puedo para de {verbo}"
