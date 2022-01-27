08d3d561c00ea0f2dc7efd7093e8d938a6fb5f5c
AbelSL <abelsl1999@gmail.com>
2022-01-26 23:19:40 -0600

    Se añadio el print de la funcion play_madlib

--- sample_madlibs/fav_mov.py
+++ sample_madlibs/fav_mov.py
@@ -1,6 +1,8 @@
-def madlib():
+def play_madlib():
     peli_fav = input("Pelicula favorita: ")
     adjetivo = input("Adjetivo: ")
     verbo = input("Verbo: ")
 
     madlibs = f"Mi pelicula favorita es {peli_fav} pues es muy {adjetivo} que no puedo para de {verbo}"
+
+    print(madlibs)
